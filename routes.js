8305b0031c9d126e9b394763348bfac52dae2925
Pedro Martins <pedrohenriquemartins@live.com>
2020-06-26 01:57:10 -0300

    created and styled create.njk

--- routes.js
+++ routes.js
@@ -1,19 +1,25 @@
-const express = require('express')
+const express = require("express");
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.redirect("instructors")
-})
+routes.get("/", (req, res) => {
+  return res.redirect("instructors");
+});
 
-routes.get('/instructors', (req, res) => {
-  return res.render("instructors/index")
-})
+routes.get("/instructors", (req, res) => {
+  return res.render("instructors/index");
+});
 
-routes.get('/members', (req, res) => {
-  return res.send("members")
-})
+routes.get("/create", (req, res) => {
+  return res.render("instructors/create");
+});
 
+routes.post("/instructors", (req, res) => {
+  return res.send(req.body);
+});
 
+routes.get("/members", (req, res) => {
+  return res.send("members");
+});
 
-module.exports = routes
+module.exports = routes;
