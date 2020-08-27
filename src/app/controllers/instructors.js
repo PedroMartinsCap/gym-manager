5163e0f31666188fff98b1f552eaf76db09081ba
Pedro Martins <pedrohenriquemartins@live.com>
2020-08-27 13:09:47 -0300

    refactoring .js files

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,132 +1,45 @@
-const { age, date } = require('../utils')
+const { age, date } = require('../../lib/utils')
 
-
-//create
-exports.create = function (req, res) {
-  return res.render('instructors/create')
-}
-
-exports.post = function (req, res) {
-  const keys = Object.keys(req.body)
-  for (key of keys) {
-    if (req.body[key] == '') {
-      console.log('this field is not filled' + req.body[key] + key)
-      return res.send('Please, fill all fields!')
+module.exports = {
+  index(req, res) {
+    return res.render('instructors/index')
+  },
+  create(req, res) {
+    return res.render('instructors/create')
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body)
+    for (key of keys) {
+      if (req.body[key] == '') {
+        console.log('this field is not filled' + req.body[key] + key)
+        return res.send('Please, fill all fields!')
+      }
     }
-  }
 
-  let { avatar_url, birth, name, services, gender } = req.body
+    let { avatar_url, birth, name, services, gender } = req.body
 
-  birth = Date.parse(birth)
-  const created_at = Date.now()
-  const id = Number(data.instructors.length + 1)
-
-  data.instructors.push({
-    id,
-    avatar_url,
-    name,
-    gender,
-    birth,
-    services,
-    created_at,
-  })
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
-    if (err) return res.redirect('/instructors')
-  })
-  return res.send('Write file error!')
-}
-
-//show
-exports.show = function (req, res) {
-  const { id } = req.params
-
-  const foundInstructor = data.instructors.find(function (instructor) {
-    return instructor.id == id
-  })
-
-  if (!foundInstructor) return res.send('Instructor not found')
-
-  const instructor = {
-    ...foundInstructor,
-    age: age(foundInstructor.birth),
-    services: foundInstructor.services.split(','),
-    created_at: new Intl.DateTimeFormat('pt-BR').format(
-      foundInstructor.created_at
-    ),
-  }
-
-  return res.render('instructors/show', { instructor })
-}
-
-//edit
-exports.edit = function (req, res) {
-  const { id } = req.params
-
-  const foundInstructor = data.instructors.find(function (instructor) {
-    return instructor.id == id
-  })
-
-  if (!foundInstructor) return res.send('Instructor not found')
-
-  const instructor = {
-    ...foundInstructor,
-    birth: date(foundInstructor.birth).iso,
-  }
-
-  return res.render('instructors/edit', { instructor })
-}
-
-//put
-exports.put = function (req, res) {
-  const { id } = req.body
-
-  let index = 0
-
-  const foundInstructor = data.instructors.find(function (
-    instructor,
-    foundIndex
-  ) {
-    if (id == instructor.id) {
-      index = foundIndex
-      return true
+    return
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  put(req, res) {
+    const keys = Object.keys(req.body)
+    for (key of keys) {
+      if (req.body[key] == '') {
+        console.log('this field is not filled' + req.body[key] + key)
+        return res.send('Please, fill all fields!')
+      }
     }
-  })
 
-  if (!foundInstructor) return res.send('Instructor not found')
+    let { avatar_url, birth, name, services, gender } = req.body
 
-  const instructor = {
-    ...foundInstructor,
-    ...req.body,
-    birth: Date.parse(req.body.birth),
-    id: Number(req.body.id)
+    return
+  },
+  delete(req, res) {
+    return
   }
-
-  data.instructors[index] = instructor
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
-    if (err) return res.send('Write error!')
-  })
-
-  return res.redirect(`/instructors/${id}`)
-}
-
-exports.delete = function (req, res) {
-  const { id } = req.body
-
-  const filteredInstructors = data.instructors.filter(function(instructor){
-    return instructor.id != id
-  })
-
-  data.instructors = filteredInstructors
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err){
-     if (err) return res.send('Write file error')
-
-     return res.redirect("/instructors")
-  })
-}
-
-exports.index = function (req, res) {
-  return res.render('instructors/index', { instructors: data.instructors} )
 }
